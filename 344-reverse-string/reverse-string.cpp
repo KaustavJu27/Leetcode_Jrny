2c48d2b7c40952e6fffde47ad3755988ae3410b5
Kaustav Mondal <kaustav709@gmail.com>
2026-07-23 13:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (53.23%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-
-        int start = 0;
-        int end = s.size() - 1;
-        while (start < end) {
-            swap(s[start++], s[end--]);
+        int maper = s.size() - 1; // Start at the last valid index
+        
+        // Loop through only the first half
+        for (int i = 0; i < s.size() / 2; i++) {
+            // Swap the front element with the back element
+            swap(s[i], s[maper]); 
+            maper--; // Move the back pointer inward
         }
     }
 };
